9bf8fc78967b7ac62bb22d5ba454ecb9322ac158
Lindsay Zhao <zhenqing@github.com>
2015-06-15 19:19:47 -0700

    update fabfile

--- cluster_fabric.py
+++ cluster_fabric.py
@@ -1,9 +1,21 @@
 from fabric.api import *
-prod_host ="52.10.23.27"
+prod_host =["172.31.18.63","172.31.24.154"]
 
 def prepare_env():
-    env.hosts = [prod_host]
-    env.username = "ubuntu"
+    env.hosts = prod_host
+    env.username = "opencfp"
 
-def prepare_deploy():
-    run("touch helloworld")
+def composer_install():
+    run("php composer.phar install")
+
+def db_migration():
+    run("vendor/bin/phinx migrate --environment=production")
+
+def run_phpunit():
+    run("./vendor/bin/phpunit")
+
+def update_app():
+    with cd('/var/www/html/opencfp'):
+        composer_install()
+        db_migration()
+        run_phpunit()
